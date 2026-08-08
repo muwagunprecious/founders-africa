fcd922874cf35129f03fe40abce76275a9ccd132
Ayomikun Ewuoso <ewuoso03@gmail.com>
2026-08-08 18:44:33 +0100

    fixed prisma configuration

---
+++ prisma/migrations/20260808174103_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "waitlist_entries" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "waitlist_entries_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "waitlist_entries_email_key" ON "waitlist_entries"("email");
